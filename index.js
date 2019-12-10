e1daf9e54774362675ac0a744d1b397409bb7e10
Richie Bendall <richiebendall@gmail.com>
2019-12-11 00:26:07 +1300

    fix: Properly support too large numbers

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const integerLimit = require("integer-limit")
 // Based on https://minecraft.gamepedia.com/Seed_(level_generation)#Technical
 
 module.exports = (seed) => {
-    if (is.string(seed) && !is.nan(Number(seed))) seed = Number(seed)
+    seed = seed.toString()
 
-    if (is.number(seed) && seed !== 0 && BigInt(seed) < integerLimit(64, { bitint: true })) return BigInt(seed)
+    if (!is.nan(Number(seed)) && seed !== 0) {
+        if (BigInt(seed) < integerLimit(64, { bitint: true })) return BigInt(seed)
+        return BigInt(hashCode(seed))
+    }
+
     if (is.string(seed)) return BigInt(hashCode(seed))
     return new JavaRandom().nextLong()
 }
--- test.js
+++ test.js
@@ -5,4 +5,6 @@ test("main", (t) => {
     t.is(mcseed(12345), 12345n)
 
     t.is(mcseed("abc"), 96354n)
+
+    t.is(mcseed(9999999999999999), mcseed(9999999999999999))
 })
